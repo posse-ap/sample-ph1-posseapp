e2d1e33fe37b528b5027ed09cc431d3dd219db04
Mitsuru Hashimoto <mitsuru_hashimoto@negocia.jp>
2021-03-06 12:31:24 +0900

    utilsのコード整理し、コメント追加

--- js/utils/index.js
+++ js/utils/index.js
@@ -8,19 +8,25 @@
      * @returns {void}
      */
     function changeWhiteSpace(targetQuery, targetParentQuery) {
+        // targetQueryに関するdom(複数)を取得
         const targetDoms = document.querySelectorAll(targetQuery)
+        // targetParentQueryに関するdom(単数)を取得し、そのdomの横幅(paddingとborderは含むが、marginは含まない)
         const parentDomWidth = document.querySelector(targetParentQuery).offsetWidth
+        // 各項目横の青ポチの幅 + margin-right
         const listDecorationWidth = 20
+        // 各項目のpadding-right
         const paddingRight = 10
         targetDoms.forEach((targetDom) => {
             // 親要素の幅 - 確認したいDOMの幅 + リストの丸ぽちの幅 + リストアイテムのpadding
+            // -> 子要素が親要素に収まる場合は何もしない
             if (parentDomWidth - targetDom.offsetWidth + listDecorationWidth + paddingRight > 0) return
+            // 子要素が親要素に収まらない場合は、子要素を折り返すようにする
             targetDom.style.whiteSpace = 'normal'
         })
     }
 
+    // ブラウザサイズが変更されると、0.25秒の猶予を持って処理を走らせる
     window.addEventListener('resize', $.throttle(250, function(){
-        changeWhiteSpace('.language', '.item-list')
-        changeWhiteSpace('.content', '.item-list')
+        changeWhiteSpace('.item', '.item-list')
     }))
 }
