999828471a554e1cf830addbd5731451db87131a
Shiori Suzuki <shiori@sgoy.jp>
2022-05-13 14:54:22 +0900

    html, css改修・chart.jsで実装

--- js/chart/index.js
+++ js/chart/index.js
@@ -1,184 +1,249 @@
 'use strict'
 
 {
-    // よく使うカラーを定義しておく
-    const WHITE = "#fff"
-    const CHART_COLOR_BLUE_1 = "#0f71bc"
-    const CHART_COLOR_BLUE_2= "#3ccfff"
+  const colors = {
+    primary: '#0f71bc',
+    primaryLight: '#97b9d1',
+    primaryBright: '#3ccfff'
+  }
+  const doughnutColors = [
+    '#0345EC',
+    '#0F71BD',
+    '#20BDDE',
+    '#3CCEFE',
+    '#B29EF3',
+    '#6D46EC',
+    '#4A17EF',
+    '#3105C0',
+  ]
 
-    const createStudyTimeChart = () => {
-        // 学習時間
-        // 2次元配列をDataTableに変更する
-        // 参考: https://developers.google.com/chart/interactive/docs/reference#arraytodatatable
-        const data = google.visualization.arrayToDataTable([
-            // roleについて https://developers.google.com/chart/interactive/docs/roles
-            ["Date", "Hour", { role: "style" } ],
-            ["", 2, CHART_COLOR_BLUE_1],
-            ["2", 8, CHART_COLOR_BLUE_2],
-            ["" , 1, CHART_COLOR_BLUE_1],
-            ["4", 2, CHART_COLOR_BLUE_2],
-            ["" , 3, CHART_COLOR_BLUE_1],
-            ["6", 4, CHART_COLOR_BLUE_2],
-            ["" , 5, CHART_COLOR_BLUE_1],
-            ["8", 6, CHART_COLOR_BLUE_2],
-            ["" , 7, CHART_COLOR_BLUE_1],
-            ["10", 1, CHART_COLOR_BLUE_2],
-            [""  , 2, CHART_COLOR_BLUE_1],
-            ["12", 3, CHART_COLOR_BLUE_2],
-            [""  , 4, CHART_COLOR_BLUE_1],
-            ["14", 7, CHART_COLOR_BLUE_2],
-            [""  , 2, CHART_COLOR_BLUE_1],
-            ["16", 7, CHART_COLOR_BLUE_2],
-            [""  , 4, CHART_COLOR_BLUE_1],
-            ["18", 3, CHART_COLOR_BLUE_2],
-            [""  , 3.2, CHART_COLOR_BLUE_1],
-            ["20", 3.5, CHART_COLOR_BLUE_2],
-            [""  , 3.2, CHART_COLOR_BLUE_1],
-            ["22", 3.5, CHART_COLOR_BLUE_2],
-            [""  , 3.2, CHART_COLOR_BLUE_1],
-            ["24", 3.5, CHART_COLOR_BLUE_2],
-            [""  , 3.2, CHART_COLOR_BLUE_1],
-            ["26", 3.5, CHART_COLOR_BLUE_2],
-            [""  , 3.2, CHART_COLOR_BLUE_1],
-            ["28", 6.5, CHART_COLOR_BLUE_2],
-            ["", 8, CHART_COLOR_BLUE_1],
-            ["30", 2, CHART_COLOR_BLUE_2],
-        ]);
-        // DataViewを作成する
-        const view = new google.visualization.DataView(data);
+  const jsonURLs = {
+    time: 'http://posse-task.anti-pattern.co.jp/1st-work/study_time.json',
+    contents: 'http://posse-task.anti-pattern.co.jp/1st-work/study_contents.json',
+    language: 'http://posse-task.anti-pattern.co.jp/1st-work/study_language.json',
+  }
 
-        const pc_options = {
-            height: '400', // 高さを指定
-            bar: { groupWidth: "50%" }, // バーの太さ
-            legend: { position: "none" }, // legendを非表示
-            vAxis:{
-                format:'0h', // 縦軸のメモリ基準
-                gridlines:{
-                    color: WHITE // 罫線の色
-                }
-            }
-        };
-        // DOMで表示場所を指定
-        const pcChart = new google.visualization.ColumnChart(document.getElementById("pc_columnchart_values"));
-        // チャートを描写
-        pcChart.draw(view, pc_options);
+  let width, height, gradient;
+  const getGradient = (ctx, chartArea) => {
+    const chartWidth = chartArea.right - chartArea.left;
+    const chartHeight = chartArea.bottom - chartArea.top;
 
-        const sp_options = {
-            height: '200', // 高さを指定
-            bar: {groupWidth: "50%"}, // バーの太さ
-            legend: { position: "none" }, // legendを非表示
-            vAxis:{
-                format:'0h', // 縦軸のメモリ基準
-                gridlines:{
-                    color:WHITE // 罫線の色
-                }
-            }
-        };
-        // DOMで表示場所を指定
-        const spChart = new google.visualization.ColumnChart(document.getElementById("sp_columnchart_values"));
-        // チャートを描写
-        spChart.draw(view, sp_options);
+    if (!gradient || width !== chartWidth || height !== chartHeight) {
+      // Create the gradient because this is either the first render
+      // or the size of the chart has changed
+      width = chartWidth;
+      height = chartHeight;
+      gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
+      gradient.addColorStop(0,colors.primary);
+      gradient.addColorStop(1, colors.primaryBright);
     }
 
-    const createLanguagesChart = () => {
-        // 学習言語
-        // 2次元配列をDataTableに変更する
-        const data = google.visualization.arrayToDataTable([
-            ['Language', 'Hour'],
-            ['HTML', 9],
-            ['CSS', 2],
-            ['JavaScript', 6],
-            ['PHP', 2],
-            ['Laravel', 7],
-            ['SQL', 5],
-            ['SHELL', 4],
-            ['情報システム基礎知識(その他)', 1],
-        ]);
+    return gradient;
+  }
 
-        const options = {
-            legend:{
-                position:"none", // legendを非表示
-            },
-            // 中心の空白部分 0~1で指定
-            pieHole: 0.5,
-            // チャートの部分ごとにカラーを指定
-            slices: {
-                0: { color: '#2222ff' },
-                1: { color: '#3344ff' },
-                2: { color: '#4466ff' },
-                3: { color: '#5588ff' },
-                4: { color: '#6699ff' },
-                5: { color: '#77aaff' },
-                6: { color: '#88ccff' },
-                7: { color: '#99ddff' },
-                8: { color: '#aaeeff' },
-            },
-            // チャートサイズ
-            chartArea: {
-                width: '100%',
-                height: '100%'
-            }
+  const htmlLegendPlugin = {
+    id: 'htmlLegend',
+    afterUpdate(chart, args, options) {
+      // const ul = getOrCreateLegendList(chart, options.containerID);
+      const ul = document.getElementById(options.containerID)
+
+      // Remove old legend items
+      while (ul.firstChild) {
+        ul.firstChild.remove();
+      }
+
+      // Reuse the built-in legendItems generator
+      const items = chart.options.plugins.legend.labels.generateLabels(chart);
+
+      items.forEach(item => {
+        const li = document.createElement('li');
+        li.classList.add('study__item')
+
+        li.onclick = () => {
+          const {type} = chart.config;
+          if (type === 'pie' || type === 'doughnut') {
+            // Pie and doughnut charts only have a single dataset and visibility is per item
+            chart.toggleDataVisibility(item.index);
+          } else {
+            chart.setDatasetVisibility(item.datasetIndex, !chart.isDatasetVisible(item.datasetIndex));
+          }
+          chart.update();
         };
 
-        // DOMで表示場所を指定
-        const chart = new google.visualization.PieChart(document.getElementById('language_piechart'));
-        // チャートを描写
-        chart.draw(data, options);
+        // Color box
+        const circle = document.createElement('span');
+        circle.classList.add('study__item__circle')
+        circle.style.background = item.fillStyle;
+
+        // Text
+        if(item.hidden) li.classList.add('--hidden')
+        const text = document.createTextNode(item.text);
+
+        li.appendChild(circle);
+        li.appendChild(text);
+        ul.appendChild(li);
+      });
     }
+  }
 
-    const createContentsChart = () => {
-        // 学習コンテンツ
-        const data = google.visualization.arrayToDataTable([
-            ['Contents', 'Hour'],
-            ['ドットインストール', 9],
-            ['N予備校', 4],
-            ['POSSE課題', 6],
-        ]);
+  const dataLabelPlugin = {
+    afterDatasetsDraw: function (chart, easing) {
+      // To only draw at the end of animation, check for easing === 1
+      var ctx = chart.ctx;
 
-        const options = {
-            legend:{
-                position:"none",
-            },
-            // 中心の空白部分 0~1で指定
-            pieHole:0.5,
-            // チャートの部分ごとにカラーを指定
-            slices: {
-                0: { color: '#2222ff' },
-                1: { color: '#66aaff' },
-                2: { color: '#aaddff' },
-            },
-            // チャートサイズ
-            chartArea: {
-                width: '100%',
-                height: '100%',
+      chart.data.datasets.forEach(function (dataset, i) {
+        let dataSum = 0;
+        dataset.data.forEach(function (element){
+          dataSum += element;
+        });
+
+        let meta = chart.getDatasetMeta(i);
+        if (!meta.hidden) {
+          meta.data.forEach(function (element, index) {
+            // Draw the text in black, with the specified font
+            ctx.fillStyle = '#fff';
+
+            var fontSize = 15;
+            var fontStyle = 'normal';
+            var fontFamily = '"Poppins", sans-serif';
+            ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily, 300);
+
+            // Just naively convert to string for now
+            // var dataString = dataset.data[index].toString() + '%';
+            let percent = Math.round(dataset.data[index] / dataSum * 1000)/10
+
+            // Make sure alignment settings are correct
+            var position = element.tooltipPosition();
+
+            if(percent >= 10) { // 10%以上で表示
+              ctx.fillText(percent + "%", position.x - (fontSize), position.y + 5);
             }
-        };
+          });
+        }
+      });
+    }
+  };
 
-        // DOMで表示場所を指定
-        const chart = new google.visualization.PieChart(document.getElementById('contents_piechart'));
-        // チャートを描写
-        chart.draw(data, options);
+  const createBarChart = (timeDay, timeHour) => {
+    const data = {
+      labels: timeDay,
+      datasets: [{
+        label: 'hour',
+        data: timeHour,
+        borderRadius: 20,
+        borderSkipped: false,
+        barPercentage: .7,
+        backgroundColor: function({chart}) {
+          const {ctx, chartArea} = chart;
+          if (!chartArea) return
+          return getGradient(ctx, chartArea);
+        },
+      }]
+    };
+
+    const config = {
+      type: 'bar',
+      data: data,
+      options: {
+        plugins: {
+          legend: false, // ラベル削除
+        },
+        scales: {
+          x: {
+            grid: {
+              color: 'transparent',
+              borderColor: 'transparent',
+              tickColor: 'transparent' // マイナス部分
+            },
+            ticks: {
+              color: colors.primaryLight,
+              maxRotation: 0, // 目盛りラベルを回転させないようにする
+            }
+          },
+          y: {
+            grid: {
+              color: 'transparent',
+              borderColor: 'transparent',
+              tickColor: 'transparent'
+            },
+            ticks: {
+              color: colors.primaryLight,
+              callback: (value, index, values) => {
+                // 奇数番目のラベルだけ消す
+                if (index % 2 === 1) return
+                return `${value}h`
+              }
+            }
+          }
+        }
+      }
+    };
+
+    new Chart(document.getElementById('js-barChart'), config)
+  }
+
+  const createStudyChart = (target, keys, values) => {
+    const data = {
+      labels: keys,
+      datasets: [{
+        data: values,
+        backgroundColor: doughnutColors,
+        borderWidth: 0
+      }]
     }
 
-    const drawChart = () => {
-        // 学習時間を表示
-        createStudyTimeChart()
-
-        // 学習言語を表示
-        createLanguagesChart()
-
-        // 学習コンテンツを表示
-        createContentsChart()
+    const config = {
+      type: 'doughnut',
+      data: data,
+      options: {
+        plugins: {
+          htmlLegend: {
+            // ID of the container to put the legend in
+            containerID: `js-legend-${target}`,
+          },
+          legend: {
+            display: false,
+          }
+        }
+      },
+      plugins: [htmlLegendPlugin, dataLabelPlugin],
     }
 
-    // パッケージのロード
-    // current: Google chart の latest versionを表す
-    // corechart: chart種類(bar, column, line, area, stepped area, bubble, pie, donut, combo, candlestick, histogram, scatter)
-    // 参照: https://developers.google.com/chart/interactive/docs/basic_load_libs
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
+    new Chart(document.getElementById(`js-studyChart-${target}`), config)
+  }
 
-    // resize時に、チャートを作り直し、windowサイズに合わせたチャートをレンダリングする
-    // drawChartが発火しすぎないようにthrottoleで0.25秒間の猶予を設けてる
-    window.addEventListener('resize', $.throttle(250, drawChart))
+  /**
+  * 学習時間　線グラフ
+  */
+  fetch(jsonURLs.time)
+  .then((response) => response.json() )
+  .then(function(json) {
+    const { timeDays, timeHours } = json.reduce((obj, element) => {
+      obj.timeDays.push(element.day)
+      obj.timeHours.push(element.time)
+      return obj
+    }, {timeDays: [], timeHours: []});
+
+    createBarChart(timeDays, timeHours)
+  })
+
+  /**
+  * 学習言語　ドーナッツチャート
+  */
+  fetch(jsonURLs.language)
+  .then((response) => response.json() )
+  .then(function(json) {
+    const obj = json[0]
+    createStudyChart('language', Object.keys(obj), Object.values(obj))
+  })
+
+  /**
+  * 学習コンテンツ　ドーナッツチャート
+  */
+  fetch(jsonURLs.contents)
+  .then((response) => response.json() )
+  .then(function(json) {
+    const obj = json[0]
+    createStudyChart('contents', Object.keys(obj), Object.values(obj))
+  })
 }
